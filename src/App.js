a29c987bdc90270286a7be72dbaf9d47a12fb80c
cankush625 <cankush625@gmail.com>
2020-11-01 02:05:04 +0530

    Tinder Card is built
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './App.css';
 import Header from "./Header";
+import TinderCards from "./TinderCards";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      {/* TinderCard */}
+      <TinderCards />
       {/* SwipeButtons */}
     </div>
   );
